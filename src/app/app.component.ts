fd8b8bb572f0a3e122594dac8226f18077072d6c
81andie <133395577+81andie@users.noreply.github.com>
2025-07-24 16:20:07 +0200

    sección sobreMi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { HeroComponent } from "./components/hero/hero.component";
 import { HaikusComponent } from "./components/haikus/haikus.component";
+import { SobreMiComponent } from "./components/sobre-mi/sobre-mi.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HeroComponent, HaikusComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, HaikusComponent, SobreMiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
