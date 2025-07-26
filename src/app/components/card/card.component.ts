d576b912e6372092e3f1cc7ab44eeada1023e4a3
81andie <133395577+81andie@users.noreply.github.com>
2025-07-27 00:17:23 +0200

    añadiendo las funcionalidades del reproductor

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,9 +1,11 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PoemasService } from '../../services/poemas.service';
 import { haikusMusicados } from '../../interfaces/poem.interface';
 import { AudioPlayerService } from '../../services/AudioPlayer.service';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PLATFORM_ID, inject, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 
 @Component({
@@ -16,11 +18,13 @@ export class CardComponent implements OnInit {
 
 
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
 
   private haikusMusicadosService = inject(AudioPlayerService)
   public haikusMusicados: haikusMusicados[] = [];
-  public haiku: haikusMusicados[] = []
+  public haiku: haikusMusicados[] = [];
+  player!: HTMLAudioElement;
+  public currentIndex: number = 0;
 
   searchTerm: string = '';
   myInput: FormControl = new FormControl('');
@@ -29,12 +33,21 @@ export class CardComponent implements OnInit {
   ngOnInit(): void {
     this.sacarAudiosyHaikus()
 
+
+    if (isPlatformBrowser(this.platformId)) {
+      this.player = new Audio();
+    }
+
+
+
+
   }
 
   sacarAudiosyHaikus() {
     this.haikusMusicadosService.getHaikusWidthAudios().subscribe((data) => {
       this.haikusMusicados = data;
       this.haiku = data;
+      this.currentIndex = 0;
       const randomIndex = Math.floor(Math.random() * data.length);
       this.haikusMusicados = [data[randomIndex]];
 
@@ -71,12 +84,73 @@ export class CardComponent implements OnInit {
         console.log(this.haikusMusicados)
 
       }
-      //this.myInput.reset()
+      // this.myInput.reset()
     }
+
   }
 
 
+  playSong(audioUrl: any) {
 
+    if (this.player) {
+      this.player.src = audioUrl;
+      this.player.play();
+    }
+
+  }
+
+  stopPlaySong(audioUrl: any) {
+    if (this.player) {
+      this.player.src = audioUrl;
+      this.player.pause()
+    }
+
+  }
+
+
+muteAudio() {
+  if (this.player) {
+    this.player.muted = true;
+    console.log('Audio muteado');
+  }
+}
+
+unmuteAudio() {
+  if (this.player) {
+    this.player.muted = false;
+    console.log('Audio activado');
+  }
+}
+
+nextSong() {
+  if (this.currentIndex < this.haikusMusicados.length - 0) {
+    this.currentIndex++;
+  } else {
+    this.currentIndex = 0; // vuelve a la primera si estás al final
+  }
+  this.haikusMusicadosService.getHaikusWidthAudios().subscribe((data) => {
+    if (data && data.length > 0) {
+      this.haikusMusicados = [data[this.currentIndex]];
+      this.playSong(this.haikusMusicados[0].audio);
+    }
+  });
+
+
+}
+
+  prevSong() {
+  if (this.currentIndex > 0) {
+    this.currentIndex--;
+  } else {
+    this.currentIndex = this.haikusMusicados.length - 0; // vuelve a la última si estás en la primera
+  }
+  this.haikusMusicadosService.getHaikusWidthAudios().subscribe((data) => {
+    if (data && data.length > 0) {
+      this.haikusMusicados = [data[this.currentIndex]];
+      this.playSong(this.haikusMusicados[2].audio);
+    }
+  });
+}
 
 
 }
