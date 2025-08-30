aef9f8d8503dcfa20ac8a56e67c371dbeff72845
81andie <133395577+81andie@users.noreply.github.com>
2025-08-30 10:13:11 +0200

    guardando los cambios en caligrama

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-
 import { InicioComponent } from "./components/inicio/inicio.component";
-import { FioriPoPupComponent } from "./components/fiori-po-pup/fiori-po-pup.component";
 import { CommonModule } from '@angular/common';
 
 
@@ -12,16 +10,15 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, NavbarComponent, FioriPoPupComponent, CommonModule],
+  imports: [RouterOutlet, RouterModule, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'fiori';
-  scrollTop(){
-  document.documentElement.scrollTop = 0;
-
- }
+  scrollTop() {
+    document.documentElement.scrollTop = 0;
+  }
 
   showChild = false; // flag para mostrar/ocultar
 
