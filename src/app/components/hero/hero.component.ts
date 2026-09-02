3bc6bfb27c7edb452772896eca42c8c3969ec5aa
81andie <133395577+81andie@users.noreply.github.com>
2026-09-02 18:10:30 +0200

    quitando js que ya no usamos en la web

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -9,53 +9,10 @@ import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
-export class HeroComponent implements OnInit{
-
-  ngOnInit(): void {
-
-   /* this.typewriter();*/
-
-  }
-
-  /*private sounds: string[] = [
-    'assets/typewriterkey1.mp3',
-    'assets/typewriter2.mp3',
-    'assets/typewriter2.mp3',
-    'assets/typewirter3.mp3',
-  ];
+export class HeroComponent {
 
 
-  public text: string = "Conoce mi blog ";
-  public displayedText: string = '';
-  private i: number = 0;
-  private speed: number = 150;
-  private timeoutId: any;
 
-  typewriter() {
-    if (this.i < this.text.length) {
-      this.displayedText += this.text.charAt(this.i);
-      this.playSound();
-      this.i++;
-      this.timeoutId = setTimeout(() => this.typewriter(), this.speed);
-    }
-  }
-
-
-  private playSound(): void {
-    if(typeof window !== 'undefined'){
-      const soundFile = this.sounds[Math.floor(Math.random() * this.sounds.length)];
-    const audio = new Audio(soundFile);
-    audio.play().catch(() => { });
-    }
-  }
-
-  ngOnDestroy(): void {
-
-    if (this.timeoutId) {
-      clearTimeout(this.timeoutId);
-    }
-  }
-*/
 
 
 }
